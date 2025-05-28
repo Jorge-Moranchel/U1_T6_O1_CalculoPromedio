3f8d559fc65d5ef0f8b11a5270e1c7905043e96c
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 19:19:08 -0600

    Eso que agregue me lo enseñaron en una empresa que estaba de software esta parte for (int j = 0; j < 5; j++) hace que repita 5 veces una por cada posicion del arreglo de eso me acuerdo mucho en si eso genera un numero random de entre 60 y 100 y este Math.random() genera el numero aleatorio entre 0.0 y 1.0 no incluye el 1 creo pero esta bien creo que si explique bien si no deberia de retomar mis mini cursos

--- promedios/src/Main.java
+++ promedios/src/Main.java
@@ -4,14 +4,18 @@ public class Main {
         //Creamos el arreglo de 5 estudiantes
         Estudiante[] estudiantes = new Estudiantes[5];
 
-        //Asignar el nmombre la matricula y notas a cada estudiantes
+        //Asignar el nombre la matricula y notas a cada estudiantes
         //Aunque se podrian asignar desde aqui bueno del programa xd
 
         for (int i = 0; i < estudiantes.lenght; i++) {
             estudiantes[i] = new Estudiante(); //Instanciar estudiantes
             estudiantes[i].setNombre("Estudiante " + (i + 1)); //Asignamos nombre
             estudiantes[i].setMatricula("Mat " + (i + 100)); //Asignamos la matricula
-            
+
+            //Asignar 5 notas aleatorias entre el 60 y 100
+            for (int j = 0; j < 5; j++) {
+                estudiantes[i].setNota(j, 60 + Math.random() * 40);
+            }
         }
     }
 }
