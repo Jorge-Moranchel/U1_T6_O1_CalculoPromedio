d11f6b0821db6be14ce1170777999b066961b5d4
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 19:12:53 -0600

    Aqui en este apartado instanciamos estudiantes y podemos asignar nombre y la matricula del estudiante vamos bien creo yo

--- promedios/src/Main.java
+++ promedios/src/Main.java
@@ -8,6 +8,9 @@ public class Main {
         //Aunque se podrian asignar desde aqui bueno del programa xd
 
         for (int i = 0; i < estudiantes.lenght; i++) {
+            estudiantes[i] = new Estudiante(); //Instanciar estudiantes
+            estudiantes[i].setNombre("Estudiante " + (i + 1)); //Asignamos nombre
+            estudiantes[i].setMatricula("Mat " + (i + 100)); //Asignamos la matricula
             
         }
     }
