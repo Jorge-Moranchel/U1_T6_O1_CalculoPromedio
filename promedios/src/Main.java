2f28d6d2c65bb1a400c3b282dfcba8dbbc5d2b7a
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 19:09:38 -0600

    Aqui hacer el arreglo de 5 estudiantes a veces no me doy a explicar bien pero se que voy por buen camino

---
+++ promedios/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //Creamos el arreglo de 5 estudiantes
+        Estudiante[] estudiantes = new Estudiantes[5];
+
+        //Asignar el nmombre la matricula y notas a cada estudiantes
+        //Aunque se podrian asignar desde aqui bueno del programa xd
+
+        for (int i = 0; i < estudiantes.lenght; i++) {
+            
+        }
+    }
+}
