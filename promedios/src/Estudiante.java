25b441974727700e0d0bed924e5178fa4b57be4f
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 18:14:40 -0600

    Ponemos atributos privados y comentarios mas que nada para que se entienda un poco mas el codigo ponemos un arreglo para guardar 5 notas

---
+++ promedios/src/Estudiante.java
@@ -0,0 +1,6 @@
+public class Estudiante {
+    // Atributos privados
+    private String nombre;
+    private String matricula;
+    private double[] promedio = new double[5]; // Arreglo para guardar 5 notas
+}
