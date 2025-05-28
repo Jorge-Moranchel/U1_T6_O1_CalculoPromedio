69bfa82378fec33e906f3d0dcceccee5b75c1bd6
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-27 18:45:58 -0600

    Usamos un método para calcular el promedio de las 5 notas de los usuarios

--- promedios/src/Estudiante.java
+++ promedios/src/Estudiante.java
@@ -3,4 +3,13 @@ public class Estudiante {
     private String nombre;
     private String matricula;
     private double[] promedio = new double[5]; // Arreglo para guardar 5 notas
+
+    // Método para calcular el promedio de las 5 notas
+    public double calcularPromedio() {
+        double suma = 0;
+        for (int i = 0; i < promedio.length; i++) {
+            suma += promedio[i]; // Sumar todas las notas
+        }
+        return suma / promedio.length; // Promedio
+    }
 }
